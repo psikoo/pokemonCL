fe9f7bdf12e758071a7e7c26fef8b3141242888b
psikoo <hugohernandezmuelas03@gmail.com>
2024-10-17 10:04:32 +0200

    changed Pokemon data to be contained in an array

--- src/main/java/com/pokemon/Pokemon.java
+++ src/main/java/com/pokemon/Pokemon.java
@@ -3,98 +3,14 @@ package com.pokemon;
 public class Pokemon { // Utility class to retrieve pokemon information
 
     public static String pokemonNameTable(int gender, int pokemon) {
-        String name = "";
-        if(gender == 0) { // Male
-            switch(pokemon) {
-                case 0:
-                    name = "Bulbasaur";
-                    break;
-                case 1:
-                    name = "Charmander";
-                    break;
-                case 2:
-                    name = "Squirtle";
-                    break;
-                default:
-                    name = "ERROR";
-            }     
-        } else if(gender == 1) { // Female
-            switch(pokemon) {
-                case 0:
-                    name = "Chikorita";
-                    break;
-                case 1:
-                    name = "Cyndaquil";
-                    break;
-                case 2:
-                    name = "Totodile";
-                    break;
-                default:
-                    name = "ERROR";
-            } 
-        } else if(gender == 2) { // Enemy
-            switch(pokemon) {
-                case 0:
-                    name = "Pikachu";
-                    break;
-                case 1:
-                    name = "Clefairy";
-                    break;
-                case 2:
-                    name = "Magikarp";
-                    break;
-                default:
-                    name = "ERROR";
-            } 
-        }
-        return name;
+        //                      Male                                    Female                                 Enemy
+        String nameArray[][] = {{"Bulbasaur", "Charmander", "Squirtle"},{"Chikorita", "Cyndaquil", "Totodile"},{"Pikachu", "Clefairy", "Magikarp"}};
+        return nameArray[gender][pokemon];
     }
 
     public static int pokemonHPTable(int gender, int pokemon) {
-        int hp = 0;
-        if(gender == 0) { // Male
-            switch(pokemon) {
-                case 0:
-                    hp = 100;
-                    break;
-                case 1:
-                    hp = 90;
-                    break;
-                case 2:
-                    hp = 95;
-                    break;
-                default:
-                    hp = 0;
-            }     
-        } else if(gender == 1) { // Female
-            switch(pokemon) {
-                case 0:
-                    hp = 90;
-                    break;
-                case 1:
-                    hp = 95;
-                    break;
-                case 2:
-                    hp = 100;
-                    break;
-                default:
-                    hp = 0;
-            } 
-        } else if(gender == 2) { // Enemy
-            switch(pokemon) {
-                case 0:
-                    hp = 60;
-                    break;
-                case 1:
-                    hp = 50;
-                    break;
-                case 2:
-                    hp = 30;
-                    break;
-                default:
-                    hp = 0;
-            } 
-        }
-        return hp;
+        //                 Male          Female        Enemy
+        int hpArray[][] = {{100, 90, 95},{90, 95, 100},{60, 50, 30}};
+        return hpArray[gender][pokemon];
     }
 }
